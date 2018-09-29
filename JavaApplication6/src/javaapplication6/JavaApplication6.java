20ec3b2c5f1c961fec73393a7cb926359c2ff67c
Lk09Nu6Uty <Lk09Nu6Uty@Lk09Nu6>
2018-09-29 16:07:35 +0700

    projek menampilkan variabel

---
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author Lk09Nu6Uty
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+        int value = 10;
+        char x;
+        x = 'A';
+       System.out.println(value);
+       System.out.println("The Values of x=" + x); 
+       // TODO code application logic here
+    }
+    
+}
